f7e8c8a4ba014550392ccf8acf7f99696a45b430
kokoA7V <oktakeru@icloud.com>
2023-09-14 14:52:05 +0900

    TitleScene作成中

--- RoomHack.ver.2.0/Assets/kokoFolder/Scripts/TitleSceneManager.cs
+++ RoomHack.ver.2.0/Assets/kokoFolder/Scripts/TitleSceneManager.cs
@@ -6,13 +6,27 @@ public class TitleSceneManager : MonoBehaviour
 {
     [SerializeField] GameObject TitleIcon;
 
+    byte iconTrans;
+
     private void Start()
     {
-        
+        iconTrans = 0;
+
+        StartCoroutine(TransUp());
     }
 
     private void Update()
     {
-        
+        TitleIcon.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, iconTrans);
+        Debug.Log(TitleIcon.GetComponent<SpriteRenderer>().color);
+    }
+
+    IEnumerator TransUp()
+    {
+        for (int i = 0; i < 255; i++)
+        {
+            yield return new WaitForSeconds(0.001f);
+            iconTrans++;
+        }
     }
 }
