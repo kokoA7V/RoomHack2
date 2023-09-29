ea87d48175c5e83fd1e55462f4ba3d7384dbf70d
Eru <eru.gamecreater@gmail.com>
2023-09-29 11:14:05 +0900

    オーディオ終わり

---
+++ RoomHack.ver.2.0/Assets/Eru/Scripts/Audio/AudioPlay.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+    [SerializeField, Header("BGMオーディオソース")]
+    private AudioSource bgmAudioSource;
+
+    [SerializeField, Header("SEオーディオソース")]
+    private AudioSource seAudioSource;
+
+    [SerializeField, Header("BGMオーディオクリップ")]
+    private AudioClip[] bgmAudioClip;
+
+    [SerializeField, Header("SEオーディオクリップ")]
+    private AudioClip[] seAudioClip;
+
+    [Header("BGMループ")]
+    public bool bgmLoop = true;
+
+    public static AudioPlay instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else Destroy(this.gameObject);
+    }
+
+    public void BGMPlay(int value)
+    {
+        bgmAudioSource.loop = bgmLoop;
+        if (value < bgmAudioClip.Length && 0 <= value) bgmAudioSource.clip = bgmAudioClip[value];
+        bgmAudioSource.Play();
+    }
+
+    public void SEPlay(int value)
+    {
+        if (value < seAudioClip.Length && 0 <= value) seAudioSource.clip = seAudioClip[value];
+        seAudioSource.Play();
+    }
+}
